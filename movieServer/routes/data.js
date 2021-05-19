a326ad2dc6b8de10980d1d9c1d45fe2a48ececd1
Matthew Meyer <mjmeyer2001@gmail.com>
2021-05-19 07:40:42 -0700

    User schema/404 page

--- movieServer/routes/data.js
+++ movieServer/routes/data.js
@@ -7,19 +7,18 @@ var path = require('path');
 //   res.send('respond with a resource');
 // });
 
-router.get("../DataTypePages/Movies.html", function (req, res) {
-  res.send("Movies.html");
+router.get("../DataTypePages/Movies.html", function(req, res) {
+    res.send("Movies.html");
 });
 
-router.get("../DataTypePages/UserData.html", function (req, res) {
-  res.sendFile("UserData.html");
+router.get("../DataTypePages/UserData.html", function(req, res) {
+    res.sendFile("UserData.html");
 });
 
-router.get("../DataTypePages/Actors.html", function (req, res) {
-  res.send("Actors.html");
+router.get("../DataTypePages/Actors.html", function(req, res) {
+    res.send("Actors.html");
 });
 
-<<<<<<< HEAD
 router.get("*", async(req, res) => {
     //res.status = 404;
     let fileLoc = path.join(__dirname, '..', 'public', '404.html')
@@ -27,6 +26,3 @@ router.get("*", async(req, res) => {
 })
 
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> f9d4055221a21d4c99592aad9420a34f7eba492d
