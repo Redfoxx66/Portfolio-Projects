8ee9559209cbfcc0928b60330d9c339d8232b7b3
Jacob <63677475+Jlimas5@users.noreply.github.com>
2021-05-26 08:34:12 -0700

    Add files via upload

--- movieServer/routes/actors.js
+++ movieServer/routes/actors.js
@@ -13,16 +13,11 @@ router.get("/", async function (req, res) {
   res.render("actorList.ejs", { actorList });
 });
 
-router.get("/actors", async function (req, res) {
-  const actorList = await Actor.find().sort("name");
-  res.render("actorList.ejs", { actorList: actorList });
-});
 
-router.get("/actors/:id", async function (req, res) {
+router.get("/id/:id", async function (req, res) {
   const actor = await Actor.findById(req.params.id);
-  res.render("actor.ejs", {
-    Actor: actor,
-  });
+  console.log(actor);
+  res.render("actor.ejs", actor);
 });
 
 module.exports = router;
