fa2dc352bcc4331b6073f9a1d67601e282d426b5
Jacob <63677475+Jlimas5@users.noreply.github.com>
2021-06-09 06:41:50 -0700

    Add files via upload

--- movieServer/public/javascripts/actorData.js
+++ movieServer/public/javascripts/actorData.js
@@ -1,186 +1,181 @@
-// const Actors = [
-//   {
-//     name: "Someone",
-//     born: "1/5/18",
-//     height: 5,
-//     twitter: "https://twitter.com/Someone",
-//     movies: ["movie1", "movie2"],
-//   },
-//   {
-//     name: "No one",
-//     born: "1/5/20",
-//     height: 8,
-//     twitter: "https://twitter.com/No-one",
-//     movies: ["movie1", "movie2"],
-//   },
-//   {
-//     name: "Someone Super important",
-//     born: "1/5/10",
-//     height: 8,
-//     twitter: "https://twitter.com/Someone-Super-important",
-//     movies: ["movie1", "movie2"],
-//   },
-//   {
-//     name: "Pig",
-//     born: "5/2/12",
-//     height: 3,
-//     twitter: "https://twitter.com/Pig",
-//     movies: ["movie3, Sprited Away"],
-//   },
-// ];
-
 const Actors = [
   //actors for spirited away
   {
     name: "Rumi Hiiragi",
-    born: "8/1/1987",
+    image: "https://image.chilimovie.com/public/300px/20200601/zITaVtFyc4xSM3mxSoPRWHbqgJI.jpg",
+    born: "1987-08-01",
     height: 5, //5' 1" (1.55 m)
-    twitter: "https://twitter.com/Rumi-Hiiragi",
+    twitter: "",
     movies: ["Spirited Away"],
   },
   {
     name: "Miyu Irino",
-    born: "2/19/1988",
+    image: "https://pbs.twimg.com/media/DCZp8vgUIAATP1j.jpg",
+    born: "1988-02-19",
     height: 5, //5' 5 3/4" (1.67 m)
-    twitter: "https://twitter.com/Miyu-Irino",
+    twitter: "https://twitter.com/miyu_irino_",
     movies: ["Spirited Away"],
   },
   {
     name: "Mari Natsuki",
-    born: "5/2/1952",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/MJK_08425_Mari_Natsuki_(Berlinale_2018).jpg/1200px-MJK_08425_Mari_Natsuki_(Berlinale_2018).jpg",
+    born: "1952-05-02",
     height: 5, //5' 4 1/4" (1.63 m)
-    twitter: "https://twitter.com/Mari-Natsuki",
+    twitter: "https://twitter.com/Marinatsukides",
     movies: ["Spirited Away"],
   },
   {
     name: "Takashi Naitô",
-    born: "5/27/1955",
+    image: "https://image.tmdb.org/t/p/original/zfs63NZKNG3Uy6WqqXSQWYMQXKZ.jpg",
+    born: "1955-05-27",
     height: 6, //6' (1.83 m)
-    twitter: "https://twitter.com/Takashi-Naitô",
+    twitter: "",
     movies: ["Spirited Away"],
   },
+
   //actors for patema inverted
   {
     name: "Yukiyo Fujii",
-    born: "5/8/1985",
+    image: "https://www.project-imas.com/w/images/3/39/Yukiyo_Fujii.jpg",
+    born: "1985-05-8",
     height: 5, //5' 6 1/2" (1.69 m)
-    twitter: "https://twitter.com/Yukiyo-Fujii",
+    twitter: "https://twitter.com/yukiyofujii0508",
     movies: ["Patema Inverted"],
   },
   {
     name: "Nobuhiko Okamoto",
-    born: "10/24/1986",
+    image: "https://otakuusamagazine.com/wp-content/uploads/2020/05/nobuhiko-okamoto.png",
+    born: "1986-10-24",
     height: 5, //5' 6¼" (1.68 m)
-    twitter: "https://twitter.com/Nobuhiko-Okamoto",
+    twitter: "",
     movies: ["Patema Inverted"],
   },
   {
     name: "Shintarô Oohata",
-    born: "10/6/1975",
+    image: "https://yukari-art.jp/site/wp-content/uploads/ohata_portrait.jpg",
+    born: "1975-10-06",
     height: 5, //probably not actually sure couldn't find
-    twitter: "https://twitter.com/Shintarô-Oohata",
+    twitter: "",
     movies: ["Patema Inverted"],
   },
+
   //actors for Avatar
   {
     name: "Sam Worthington",
-    born: "9/2/1976",
+    image: "https://movie-fanatic-res.cloudinary.com/iu/s--FNZ-cqHi--/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1364991325/australian-actor-sam-worthington.jpg",
+    born: "1976-09-02",
     height: 5, //5' 10" (1.78 m)
-    twitter: "https://twitter.com/Sam-Worthington",
+    twitter: "https://twitter.com/_samworthington",
     movies: ["Avatar", "Hacksaw Ridge"],
   },
   {
     name: "Zoe Saldana",
-    born: "6/19/1978",
+    image: "https://celebmafia.com/wp-content/uploads/2019/02/zoe-saldana-science-fair-tv-show-panel-at-tca-winter-press-tour-in-la-02-10-2019-2.jpg?x77703",
+    born: "1978-06-19",
     height: 5, //5' 7" (1.7 m)
-    twitter: "https://twitter.com/Zoe-Salanda",
+    twitter: "https://twitter.com/zoesaldana",
     movies: ["Avatar"],
   },
   {
     name: "Sigourney Weaver",
-    born: "10/8/1949",
+    image: "https://www.newdvdreleasedates.com/images/profiles/sigourney-weaver-08.jpg",
+    born: "1949-10-08",
     height: 6, //6' 1" (1.85 m)
-    twitter: "https://twitter.com/Sigourney-Weaver",
+    twitter: "",
     movies: ["Avatar"],
   },
   {
     name: "Michelle Rodriguez",
-    born: "7/12/1978",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Michelle_Rodriguez_by_Gage_Skidmore_2.jpg/1200px-Michelle_Rodriguez_by_Gage_Skidmore_2.jpg",
+    born: "1978-07-12",
     height: 5, //5' 5" (1.65 m)
-    twitter: "https://twitter.com/Michelle-Rodriguez",
+    twitter: "https://twitter.com/MRodOfficial",
     movies: ["Avatar"],
   },
+
   //actors for Hacksaw Ridge
   {
     name: "Andrew Garfield",
-    born: "9/20/1983",
+    image: "https://www.newdvdreleasedates.com/images/profiles/andrew-garfield-08.jpg",
+    born: "1983-09-20",
     height: 5, //5' 10½" (1.79 m)
-    twitter: "https://twitter.com/Andrew-Garfield",
+    twitter: "https://twitter.com/Itsagarfield",
     movies: ["Hacksaw Ridge"],
   },
   {
     name: "Hugo Weaving",
-    born: "4/4/1960",
+    image: "http://www.media2.hw-static.com/media/2018/12/wenn_mortalengines_premiere_hugoweaving_120618.jpg",
+    born: "1960-04-04",
     height: 6, //6' 2" (1.88 m)
-    twitter: "https://twitter.com/Hugo-Weaving",
+    twitter: "",
     movies: ["Hacksaw Ridge"],
   },
   {
     name: "Teresa Palmer",
-    born: "2/26/1986",
+    image: "http://celebmafia.com/wp-content/uploads/2017/01/teresa-palmer-sag-awards-in-los-angeles-1-29-2017-2.jpg",
+    born: "1986-02-26",
     height: 5, //5' 4½" (1.64 m)
-    twitter: "https://twitter.com/Teresa-Palmer",
+    twitter: "https://twitter.com/teresapalmer",
     movies: ["Hacksaw Ridge"],
   },
+
   //actors for Hot Rod
   {
     name: "Andy Samberg",
-    born: "8/18/1978",
+    image: "https://images.hellogiggles.com/uploads/2018/05/15070822/andy-samberg-brooklyn-nine-nine.jpg",
+    born: "1978-08-18",
     height: 5, //5' 9½" (1.77 m)
-    twitter: "https://twitter.com/Andy-Samberg",
+    twitter: "https://twitter.com/thelonelyisland",
     movies: ["Hot Rod"],
   },
   {
     name: "Isla Fisher",
-    born: "2/3/1976",
+    image: "https://www.celebskart.com/wp-content/uploads/2018/12/isla-fisher-at-refinery29s-29rooms-los-angeles-2018-expand-your-reality-2018-12-04-02.jpg",
+    born: "1976-02-03",
     height: 5, //5' 3" (1.6 m)
-    twitter: "https://twitter.com/Isla-Fisher",
+    twitter: "https://twitter.com/islafisherweb",
     movies: ["Hot Rod"],
   },
   {
     name: "Ian McShane",
-    born: "9/29/1942",
+    image: "https://superstarsbio.com/wp-content/uploads/2019/09/ian-mcsgods-1024x683.jpg",
+    born: "1942-09-29",
     height: 5, //5' 7½" (1.71 m)
-    twitter: "https://twitter.com/Ian-McShane",
+    twitter: "",
     movies: ["Hot Rod", "Kung Fu Panda"],
   },
   {
     name: "Bill Hader",
-    born: "6/7/1978",
+    image: "https://sharpmagazine.com/wp-content/uploads/2019/08/BillHader_SharpMagazine_0869-v3.jpg",
+    born: "1978-06-07",
     height: 6, //6' 1" (1.85 m)
-    twitter: "https://twitter.com/Bill-Hader",
+    twitter: "",
     movies: ["Hot Rod"],
   },
+
   //actors for Kung Fu Panda
   {
     name: "Jack Black",
-    born: "8/28/1969",
+    image: "https://www.themodestman.com/wp-content/uploads/2020/05/Jack-Black.jpg",
+    born: "1969-08-28",
     height: 5, //5' 6" (1.68 m)
-    twitter: "https://twitter.com/Jack-Black",
+    twitter: "https://twitter.com/jackblack",
     movies: ["Kung Fu Panda"],
   },
   {
     name: "Angelina Jolie",
-    born: "6/4/1975",
+    image: "https://vignette.wikia.nocookie.net/disney/images/9/90/Angelina_Jolie.jpg/revision/latest?cb=20180604002246",
+    born: "1975-06-04",
     height: 5, //5' 6½" (1.69 m)
-    twitter: "https://twitter.com/Angelina-Jolie",
+    twitter: "https://twitter.com/angelinajollyi",
     movies: ["Kung Fu Panda"],
   },
   {
     name: "Dustin Hoffman",
-    born: "8/8/1937",
+    image: "https://images.thestar.com/AXJCQh5okDq9b-IWsmNZjHfvoaw=/1086x1429/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/entertainment/2012/05/08/dustin_hoffman_helps_save_london_joggers_life/dustinhoffman.jpeg",
+    born: "1937-08-08",
     height: 5, //5' 5¾" (1.67 m)
-    twitter: "https://twitter.com/Dustin-Hoffman",
+    twitter: "https://twitter.com/dustinhoffman3",
     movies: ["Kung Fu Panda"],
   },
 ];
